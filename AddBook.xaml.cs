0408e0904120019a2420489a9cda5a27270668e6
Zaha Fatima zahra <fatimazahra.zaha1504@gmail.com>
2023-12-24 14:52:45 +0100

    Ajoutez des fichiers projet.

---
+++ AddBook.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryAdmin
+{
+    /// <summary>
+    /// Logique d'interaction pour AddBook.xaml
+    /// </summary>
+    public partial class AddBook : Window
+    {
+        public AddBook()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Home home = new Home();
+            home.Show();
+            this.Close();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Home home = new Home();
+            home.Show();
+            this.Close();
+        }
+
+        private void SelectFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Images (*.jpg;*.jpeg;*.png;*.gif)|*.jpg;*.jpeg;*.png;*.gif|Tous les fichiers (*.*)|*.*"; // Filtre les types de fichiers affichés
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                // Traitez le fichier sélectionné ici, par exemple, affichez le chemin d'accès dans une étiquette ou chargez le contenu du fichier.
+                FilePathLabel.Content = selectedFilePath;
+            }
+        }
+    }
+}
---
+++ Home.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryAdmin
+{
+    /// <summary>
+    /// Logique d'interaction pour Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var connexion = new MainWindow(); //create your new form.
+            
+            connexion.Show(); //show the new form.
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AddBook addBook = new AddBook();
+            addBook.Show();
+            this.Close();
+        }
+
+        private void Add_Book(object sender, RoutedEventArgs e)
+        {
+            EditBook editBook = new EditBook();
+            editBook.Show();
+            this.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
